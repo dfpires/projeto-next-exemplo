9d2ed9981243f7bb108e138d46fc8ca9ef5ef385
dfpires@gmail.com <dfpires@gmail.com>
2022-02-23 22:17:21 -0300

    23-02

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
+import style from './styles.module.scss'
+
 export function Header() {
     return (
-        <header>
-            <div>
+        <header className={style.container}>
+            <div className={style.content}>
                 <img src="/logo.svg" alt="Dev News"/>
                 <nav>
                     <Link href="/"> 

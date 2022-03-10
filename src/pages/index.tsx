1180f4a49c67f3a22fe99512e5095b9e80ece68b
Professor <professor@unifacef.edu.br>
2022-03-09 22:31:29 -0300

    finalizando header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
-
 
 interface Post {
   id: string,
@@ -10,20 +7,30 @@ interface Post {
 
 export default function Home() {
   // variável de estado
-  const [posts, setPosts] = useState<Post[]>()
+  const [posts, setPosts] = useState<Post[]>([])
 
   // executado quando o componente é carregado apenas porque não tem variável sendo controlada []
   useEffect( () => {
     fetch('http://localhost:3333/posts') // vai no servidor
-    .then(response => { // espera a resposta do servidor
-      response.json() // transforma em json
-      .then( data => { // espera transformar a resposta em json
-        setPosts(data) // atualiza vetor de posts
+      .then(response => { // espera a resposta do servidor
+        response.json() // transforma em json
+        .then( data => { // espera transformar a resposta em json
+          setPosts(data) // atualiza vetor de posts
       })
     })
   }, [])
 
   return (
-    <h1 > Olá <span> Dev </span> </h1>
+    <div>
+      <h1> Posts </h1>
+      <ul>
+        {
+          posts.map( post => (
+             <li key={post.id}> {post.title} </li>
+          ))
+        }
+      </ul>
+    </div>
+    
   )
 }

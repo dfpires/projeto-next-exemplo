10a560997346d6b54dd6872398303441132da122
Professor <professor@unifacef.edu.br>
2022-03-09 21:57:50 -0300

    finalizando header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+
+interface Post {
+  id: string,
+  title: string
+}
 
 export default function Home() {
+  // variável de estado
+  const [posts, setPosts] = useState<Post[]>()
+
+  // executado quando o componente é carregado apenas porque não tem variável sendo controlada []
+  useEffect( () => {
+    fetch('http://localhost:3333/posts')
+    
+  }, [])
+
   return (
-         
     <h1 > Olá <span> Dev </span> </h1>
   )
 }

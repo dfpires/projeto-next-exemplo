c54e4f46adaa2c4806388814e35e3223bc06d97f
Professor <professor@unifacef.edu.br>
2022-03-16 21:22:47 -0300

    Server Side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,22 @@
-import { useEffect, useState } from 'react'
+import {GetServerSideProps} from 'next'
 
 interface Post {
-  id: string,
-  title: string
+  id: string;
+  title: string;
+}
+
+interface Posts {
+  posts: Post[]
 }
 
 export default function Home() {
-  // variável de estado
-  const [posts, setPosts] = useState<Post[]>([])
-
-  // executado quando o componente é carregado apenas porque não tem variável sendo controlada []
-  useEffect( () => {
-    fetch('http://localhost:3333/posts') // vai no servidor
-      .then(response => { // espera a resposta do servidor
-        response.json() // transforma em json
-        .then( data => { // espera transformar a resposta em json
-          setPosts(data) // atualiza vetor de posts
-      })
-    })
-  }, [])
-
   return (
     <div>
-      <h1> Posts </h1>
-      <ul>
-        {
-          posts.map( post => (
-             <li key={post.id}> {post.title} </li>
-          ))
-        }
-      </ul>
-    </div>
     
+    </div>
   )
+}
+// função será executada antes do componente ser carregado
+export const getServerSideProps: GetServerSideProps<> = async () => {
+
 }

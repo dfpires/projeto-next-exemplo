3a56194fd41987d131facf74181e338895245f27
Professor <professor@unifacef.edu.br>
2022-03-16 21:55:24 -0300

    Server Side

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,23 @@
+import {GetStaticProps} from 'next'
+interface Post { // representa 1 post
+    id: string;
+    title: string;
+}
+  
+interface Posts { // representa vetor de posts
+    posts: Post[]
+}
+
 export default function Posts(){
     return <h1> Posts </h1>
+}
+
+export const getStaticProps: GetStaticProps<Posts> = async() => {
+  const response = await fetch('http://localhost:3333/posts')
+  const posts = await response.json()
+  return {
+    props: {
+      posts // posts vai para o componente
+    }
+  }
 }

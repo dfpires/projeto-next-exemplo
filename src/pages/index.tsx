70857b2a1524ac75042b6bea5901f6dd7b107a93
Your Name <you@example.com>
2022-05-04 21:25:13 -0300

    04-05

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,21 @@ export default function Home() {
   return (
     <>
       <SEO title="Dev News" excludeTitleSuffix/>
-
-      <h1> Posts - teste</h1>
+      <main>
+        <section>
+          <span> Olá Dev ! </span>
+          <h1>
+            Bem-vindo e bem-vinda <br/>
+            ao <span> Dev </span> News !
+          </h1>
+          <p>
+            Um blog com conteúdos extremamente <br/>
+            <span> relevantes para o seu aprendizado </span>
+          </p> 
+        </section>
+        <img src="/home.svg" alt="Home image"/>
+      </main>
+      
       
     </>
   )
